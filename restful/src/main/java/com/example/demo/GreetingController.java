7078386114c34c3817adacae63c116ef317102be
Carlos Torres Sanchez <ing.torres.carlos@gmail.com>
2019-08-16 17:50:11 -0500

    Add eureka service registry

--- restful/src/main/java/com/example/demo/GreetingController.java
+++ restful/src/main/java/com/example/demo/GreetingController.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import java.net.URI;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class GreetingController {
@@ -14,15 +18,18 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestBody  Greeting greeting) {
+    public ResponseEntity<Void> greeting(@RequestBody  Greeting greeting) {
     	System.out.println("RESPONDED");
     	System.out.println("Email"+greeting.getEmail());
     	System.out.println(greeting.getMessage());
     	System.out.println(greeting.getStarter());
     	System.out.println(greeting.getOrigin());
     	System.out.println(greeting.getName());
+    	return ResponseEntity.status(HttpStatus.FOUND)
+    	        .location(URI.create("http://localhost:8086/Scheduled.html"))
+    	        .build();
 
-    	return new Greeting();
+    	
     }
     
     
